2c62ef83b4b130c38830d1be683da5ccdb9c2662
LugburZ-BUV <Nils-olmas@live.se>
2026-09-15 14:27:22 +0200

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Ovningar_09_15_convert_class
+{
+    public class Person
+    {
+        // First name, last name, and age for class person
+        public string firstName {  get; set; }
+        public string lastName {  get; set; }
+        public int Age { get; set; }
+
+        public Person (string firstname, string lastname, int age)
+        {
+            firstName = firstname;
+            lastName = lastname;
+            Age = age;
+        }
+        public bool MayHaveDriversLicense()
+        {
+            if (this.Age >= 18)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Name: {firstName} {lastName}" +
+                $"\nAge: {Age}");
+            Console.WriteLine(MayHaveDriversLicense());
+        }
+        public void Birthday()
+        {
+            this.Age++;
+        }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Ovningar_09_15_convert_class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Nils = new Person("Nils","Olmås",30);
+            Person Kalle = new Person("Kalle", "Anka", 70);
+
+            Kalle.PrintInfo();
+            Nils.PrintInfo();
+
+            Nils.Birthday();
+
+            Nils.PrintInfo();
+        }
+    }
+}
